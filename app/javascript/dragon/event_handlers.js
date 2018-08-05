af2ea24c35bd626adb89b95359989c5bae5f75a2
KTOKH <lavender30k@icloud.com>
2018-08-05 09:44:48 +0900

    1st commit

--- app/javascript/dragon/event_handlers.js
+++ app/javascript/dragon/event_handlers.js
@@ -63,19 +63,38 @@ function SpaceKeydownOnCave(scene, event) {
 }
 
 function ArrowLeft(scene, event) {
-  scene._player.setFrame(0)
-  if (movable_to(scene, -1, 0)) scene._player._x--
+  if (scene._player.frame.name === CONSTANTS.LEFT_1)
+    scene._player.setFrame(CONSTANTS.LEFT_2)
+  else
+    scene._player.setFrame(CONSTANTS.LEFT_1)
+
+  if (movable_to(scene,-1,0)) scene._player._x--
 }
 
 function ArrowRight(scene, event) {
-  scene._player.setFrame(1)
-  if (movable_to(scene, 1, 0)) scene._player._x++
+  if (scene._player.frame.name === CONSTANTS.RIGHT_1)
+    scene._player.setFrame(CONSTANTS.RIGHT_2)
+ else
+    scene._player.setFrame(CONSTANTS.RIGHT_1)
+ 
+    if (movable_to(scene,1,0)) scene._player._x++
 }
 
 function ArrowUp(scene, event) {
+  if (scene._player.frame.name === CONSTANTS.BACK_1)
+    scene._player.setFrame(CONSTANTS.BACK_2)
+  else
+    scene._player.setFrame(CONSTANTS.BACK_1)
+
   if (movable_to(scene, 0, -1)) scene._player._y--
 }
 
+
 function ArrowDown(scene, event) {
-  if (movable_to(scene, 0, 1)) scene._player._y++
+  if (scene._player.frame.name === CONSTANTS.FRONT_2)
+    scene._player.setFrame(CONSTANTS.FRONT_2)
+  else
+   scene._player.setFrame(CONSTANTS.FRONT_1)
+  
+   if (movable_to(scene,0,1)) scene._player._y++
 }
